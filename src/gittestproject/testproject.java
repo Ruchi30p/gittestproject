d22f68ff9ab364f8a51989983f1433b671f08243
ruchi <ruchi@Ruchi>
2023-03-19 02:54:09 -0400

    First Commit

---
+++ src/gittestproject/testproject.java
@@ -0,0 +1,14 @@
+package gittestproject;
+
+public class testproject {
+int marks;
+	public static void main(String[] args) {
+		String hungry ="yes";
+		
+	if (hungry=="yes")
+		System.out.println("Eat something");
+	else {System.out.println("Work");}
+
+	}
+
+}
